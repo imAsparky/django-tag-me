02057aa39b92ddf8fed4cad2883c065b72e43d34
mark sevelj <31756570+imAsparky@users.noreply.github.com>
2023-09-24 14:11:41 +0800

    test(tags): Add collection and model field tests #7 (#8)
    * test(field): Update model field and add tests #7
    
    While adding tests several improvements where found in `TagMeCharField`.
    
    closes #7
    
    * test(collection): Update collections and add tests #7
    
    While adding tests several improvements where found in `FieldTagListFormatter`.
    
    closes #7

--- tests/test_collections.py
+++ tests/test_collections.py
@@ -45,6 +45,51 @@ class TestTagCollectionsFieldTagListFormatter:
         assert obj.count("one") == 1
         assert obj.count("two") == 1
 
+    def test__add__FieldTagListFormatter(self):
+        a = FieldTagListFormatter("a,b,c")
+        b = FieldTagListFormatter("d,e,f")
+        c = a + b
+
+        d = FieldTagListFormatter("a,b,c,d,e,f,x,y,z")
+        e = c + d
+
+        assert c == ["a", "b", "c", "d", "e", "f"]
+        assert e == ["a", "b", "c", "d", "e", "f", "x", "y", "z"]
+
+    def test__add__list(self):
+        a = FieldTagListFormatter("a,b,c")
+        b = ["d", "e", "f"]
+        c = a + b
+
+        d = ["a", "b", "c", "d", "e", "f", "x", "y", "z"]
+        e = c + d
+        assert c == ["a", "b", "c", "d", "e", "f"]
+        assert e == ["a", "b", "c", "d", "e", "f", "x", "y", "z"]
+
+    def test__add__str(self):
+        a = FieldTagListFormatter("a,b,c")
+        b = "d"
+        c = "a"
+        d = a + b + c
+
+        assert d == ["a", "b", "c", "d"]
+
+    def test__radd__str(self):
+        a = FieldTagListFormatter("a,b,c")
+        b = FieldTagListFormatter("d,e,f")
+        c = ["d", "e", "f"]
+        d = "d"
+
+        assert a.__radd__(b) == ["a", "b", "c", "d", "e", "f"]
+        assert a.__radd__(c) == ["a", "b", "c", "d", "e", "f"]
+        assert a.__radd__(d) == ["a", "b", "c", "d"]
+
+    def test_add_None_returns_unchanged(self):
+        obj = FieldTagListFormatter("a,b,c")
+        obj.add_tags(None)
+
+        assert obj == ["a", "b", "c"]
+
     # *************************    Append Tags     ****************************
 
     def test_append_tags_dict_of_list_no_dups(self):
@@ -113,6 +158,12 @@ class TestTagCollectionsFieldTagListFormatter:
         assert obj.count("three") == 1
 
     # **************************    Del Tags   ********************************
+
+    def test__delitem__(self):
+        obj = FieldTagListFormatter("a,b,c")
+        obj.__delitem__(2)
+
+        assert obj == ["a", "b"]
 
     def test_del_tags_dict_of_list(self):
         obj = FieldTagListFormatter(["one", "two", "three"])
@@ -204,7 +255,7 @@ class TestTagCollectionsFieldTagListFormatter:
             FieldTagListFormatter(objs)
 
         assert (
-            "must be dict or list or set containing strings, or a string or None, type is"
+            "must be dict or list or set containing strings, or a string or None, type is"  # noqa: E501
             in str(exc.value)
         )
         assert exc.type == ValidationError
@@ -231,7 +282,30 @@ class TestTagCollectionsFieldTagListFormatter:
             "\\", ""
         )
         assert exc.type == ValidationError
-        # assert 1 == 1
+
+    def test_unsupported_type_in_dict_raises_error(self):
+        obj = FieldTagListFormatter("a,b,c")
+        obj2 = {"tags": {"error": "here"}}
+        with pytest.raises(ValidationError) as exc:
+            obj.add_tags(obj2)
+        assert (
+            "{'tags': {'error': 'here'}} The field dict must contain a value type str or set[str] or list[str]. Keys supplied dict_keys(['tags']). Value type <class 'dict'>."  # noqa: E501
+            in str(exc.value)
+        )
+
+        assert exc.type == ValidationError
+
+    def test_missing_key_in_dict_raises_error(self):
+        obj = FieldTagListFormatter("a,b,c")
+        obj2 = {"error": "a,b,c"}
+        with pytest.raises(ValidationError) as exc:
+            obj.add_tags(obj2)
+        assert (
+            "{'error': 'a,b,c'} The field dict must contain the key 'tags' with a value type str or set[str] or list[str]. Keys supplied dict_keys(['error'])."  # noqa: E501
+            in str(exc.value)
+        )
+
+        assert exc.type == ValidationError
 
     # *************************    Extend Tags     ****************************
 
@@ -398,6 +472,121 @@ class TestTagCollectionsFieldTagListFormatter:
         assert obj.count("two") == 1
         assert obj.count("three") == 1
 
+    # ****************     Other Tag  and __ Methods    ***********************
+
+    def test__contains__(self):
+        obj = FieldTagListFormatter("a,b,c")
+
+        assert "a" in obj
+
+    def test_copy(self):
+        obj = FieldTagListFormatter("a,b,c")
+        obj2 = obj.copy()
+
+        assert obj == obj2
+
+    def test_clear(self):
+        obj = FieldTagListFormatter("a,b,c")
+        obj.clear()
+
+        assert obj.tags == []
+
+    def test__ge__(self):
+        obj = FieldTagListFormatter("a,b,c")
+        obj2 = obj.copy()
+        obj3 = FieldTagListFormatter("a,b")
+
+        assert obj.__ge__(obj2)
+        assert obj.__ge__(obj3)
+
+    def test__getitem__(self):
+        obj = FieldTagListFormatter("a,b,c,d,e,f,g,h")
+
+        assert obj[3:5] == ["d", "e"]
+        assert obj[6] == "g"
+
+    def test__gt__(self):
+        obj = FieldTagListFormatter("a,b,c")
+        obj2 = FieldTagListFormatter("a,b")
+
+        assert obj.__gt__(obj2)
+
+    def test__iad__(self):
+        obj = FieldTagListFormatter("a,b,c")
+        obj2 = FieldTagListFormatter("d,e,f")
+        obj3 = ["d", "e", "f"]
+
+        assert obj.__iadd__(obj2) == ["a", "b", "c", "d", "e", "f"]
+
+        obj = FieldTagListFormatter("a,b,c")
+        obj3 = ["d", "e", "f"]
+        assert obj.__iadd__(obj3) == ["a", "b", "c", "d", "e", "f"]
+
+        obj = FieldTagListFormatter("a,b,c")
+        obj4 = "z"
+        assert obj.__iadd__(obj4) == ["a", "b", "c", "z"]
+
+    def test__imul__(self):
+        obj = FieldTagListFormatter("a,b,c")
+
+        assert obj.__imul__(2) == ["a", "b", "c", "a", "b", "c"]
+
+    def test_index(self):
+        obj = FieldTagListFormatter("a,b,c")
+
+        assert obj.index("b") == 1
+
+    def test__le__(self):
+        obj = FieldTagListFormatter("a,b,c")
+        obj2 = obj.copy()
+        obj3 = FieldTagListFormatter("a,b,c,d")
+
+        assert obj.__le__(obj2)
+        assert obj.__le__(obj3)
+
+    def test__len__(self):
+        obj = FieldTagListFormatter("a,b,c")
+
+        assert obj.__len__() == 3
+
+    def test__lt__(self):
+        obj = FieldTagListFormatter("a,b,c")
+        obj2 = FieldTagListFormatter("a,b,c,d")
+
+        assert obj.__lt__(obj2)
+
+    def test__mul__(self):
+        obj = FieldTagListFormatter("a,b,c")
+
+        assert obj.__mul__(2) == ["a", "b", "c", "a", "b", "c"]
+
+    def test_pop(self):
+        obj = FieldTagListFormatter("a,b,c,d,e,f")
+
+        # Test default removes the last item
+        obj.pop()
+        assert obj == ["a", "b", "c", "d", "e"]
+
+        obj.pop(0)
+        assert obj == ["b", "c", "d", "e"]
+
+    def test__repr__(self):
+        a = FieldTagListFormatter("a,b,c")
+
+        assert a.__repr__() == "['a', 'b', 'c']"
+
+    def test_reverse(self):
+        obj = FieldTagListFormatter("a,b,c,d,e,f")
+        obj.reverse()
+
+        assert obj == ["f", "e", "d", "c", "b", "a"]
+
+    def test_sort(self):
+        obj = FieldTagListFormatter("f,d,e,a,c,b")
+        obj.sort()
+
+        assert obj == ["a", "b", "c", "d", "e", "f"]
+
     # ************************    Remove Tag    *******************************
 
     def test_remove_item(self):
@@ -415,7 +604,7 @@ class TestTagCollectionsFieldTagListFormatter:
         assert obj.count("two") == 1
         assert obj.count("three") == 1
 
-    # ********************s******    Set Tag    ***********s*********************
+    # ********************s******    Set Tag    *******************************
 
     def test_setitem_no_dups(self):
         obj = FieldTagListFormatter("one, two, three")
