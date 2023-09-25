5585df2c8dcf9b290c4e217f7ec1ac39dab3544a
Mark Sevelj <mark.sevelj@dunwright.com.au>
2023-09-25 12:02:45 +0800

    test(tags):Add form field tests #14
    While adding tests several improvements where found in
    `TagMeCharFieldForm`.
    
    closes #14

--- tests/test_form_field.py
+++ tests/test_form_field.py
@@ -1,12 +1,13 @@
 """tag-me model field tests"""
 
+from django.core import validators
 from django.test import SimpleTestCase
 
 from tag_me.db.forms.fields import TagMeCharFieldForm
 from tag_me.utils.collections import FieldTagListFormatter
 
 
-class TestTagMeCharFieldFormToPython(SimpleTestCase):
+class TestTagMeCharFieldForm(SimpleTestCase):
     def test_empty_value(self):
         f = TagMeCharFieldForm()
 
@@ -17,53 +18,26 @@ class TestTagMeCharFieldFormToPython(SimpleTestCase):
 
         assert f.to_python("a,b,c") == FieldTagListFormatter("a,b,c").toCSV()
 
+    def test_validators(self):
+        f = TagMeCharFieldForm(
+            min_length=1234,
+            max_length=5678,
+        )
 
-# After refactor these tests are redundant, here for reference. Can be deleted
-# class TestTagMeCharFieldFormValidators(SimpleTestCase):
-#     def test_no_min_max_but_default_validators(self):
-#         cf = TagMeCharFieldForm()
+        assert any(
+            x
+            for x in f.validators
+            if isinstance(x, validators.MaxLengthValidator)
+        )
 
-#         assert not any(
-#             x
-#             for x in cf.validators
-#             if isinstance(x, validators.MaxLengthValidator)
-#         )
-#         assert not any(
-#             x
-#             for x in cf.validators
-#             if isinstance(x, validators.MinLengthValidator)
-#         )
-#         assert any(
-#             x
-#             for x in cf.validators
-#             if isinstance(x, validators.ProhibitNullCharactersValidator)
-#         )
+        assert any(
+            x
+            for x in f.validators
+            if isinstance(x, validators.MinLengthValidator)
+        )
 
-#     def test_max_length_and_default_validator(self):
-#         cf = TagMeCharFieldForm(max_length=1234)
-
-#         assert any(
-#             x
-#             for x in cf.validators
-#             if isinstance(x, validators.MaxLengthValidator)
-#         )
-
-#         assert any(
-#             x
-#             for x in cf.validators
-#             if isinstance(x, validators.ProhibitNullCharactersValidator)
-#         )
-
-#     def test_min_length_and_default_validator(self):
-#         cf = TagMeCharFieldForm(min_length=5)
-
-#         assert any(
-#             x
-#             for x in cf.validators
-#             if isinstance(x, validators.MinLengthValidator)
-#         )
-#         assert any(
-#             x
-#             for x in cf.validators
-#             if isinstance(x, validators.ProhibitNullCharactersValidator)
-#         )
+        assert any(
+            x
+            for x in f.validators
+            if isinstance(x, validators.ProhibitNullCharactersValidator)
+        )
