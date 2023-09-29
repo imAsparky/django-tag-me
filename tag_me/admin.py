99bdc7774a1b2caf984d521757547cd688ac6294
Mark Sevelj <mark.sevelj@dunwright.com.au>
2023-09-29 08:39:15 +0800

    chore(admin): Register models #37
    closes #37

--- tag_me/admin.py
+++ tag_me/admin.py
@@ -2,4 +2,9 @@
 
 from django.contrib import admin
 
+from tag_me.models import TaggedFieldModel, UserTag
+
 # Register your tag_me models here.
+
+admin.site.register(TaggedFieldModel)
+admin.site.register(UserTag)
