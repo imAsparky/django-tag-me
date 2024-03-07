4958ce5d3e1f325804140d33a2f3d202aaa6f37a
imAsparky <mark.sevelj@dunwright.com.au>
2024-03-07 11:13:47 +0800

    refactor(util): Update collections #44
    Refactor collections to improve readability.
    Add docstrings
    Add type hint to parser output.
    
    closes #44

--- tag_me/utils/collections.py
+++ tag_me/utils/collections.py
@@ -3,13 +3,13 @@
 # from collections.abc import MutableSequence
 
 import json
+import logging
+import re
 
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
 from tag_me.utils.parser import parse_tags
-
-x = list
 
 
 class FieldTagListFormatter(list):
@@ -25,14 +25,20 @@ class FieldTagListFormatter(list):
     examples of how strings of tags are treated using `parse_tags`.
     """
 
+    logger = logging.getLogger(__name__)
+
     def __init__(
         self,
-        tags: dict[str : list[str] | set[str] | str]  # noqa: E203
-        | list[str]
-        | set[str]
-        | str
-        | None = None,
-    ) -> list[str]:
+        tags: (
+            dict[str : list[str] | set[str] | str]  # noqa: E203
+            | list[str]
+            | set[str]
+            | str
+            | None
+        ) = None,
+    ) -> None:
+
+        super().__init__()
         self.tags = []
         self.add_tags(tags)
 
@@ -123,30 +129,73 @@ class FieldTagListFormatter(list):
         if item not in self.tags:
             self.tags[i] = item
 
-    def _get_tag_list(
-        self,
-        tags: dict[str : list[str] | set[str] | str]  # noqa: E203
-        | list[str]
-        | set[str]
-        | str
-        | None = None,
-    ) -> list[str]:
-        """Takes multiple input types and returns a list of tags."""
+    @staticmethod
+    def _is_valid_tag(tag: str) -> bool:
+        """Checks if a tag is a valid string and not a null value."""
+        null_pattern = re.compile(r"\bnull\b[\.,]?", re.IGNORECASE)
+        return isinstance(tag, str) and not null_pattern.match(tag)
+
+    @staticmethod
+    def _is_valid_tag_container(tags: dict | list | set | str | None) -> bool:
+        """
+        Checks if the tag container is a valid type and, if a dict, contains
+        the 'tags' key.
+
+        Raises:
+            ValidationError: If the tag container is a dict and doesn't have
+                            the 'tags' key.
+        """
         match tags:
             case dict():
-                # If there is a "tags" key, passing the "tags" values to
-                # FieldTagList ensures that it passes validation.
-                if "tags" in tags.keys():
-                    match tags["tags"]:
-                        case list() | set():
-                            return list(set(tags["tags"]))
-                        case str():
-                            return parse_tags(tags["tags"])
+                if "tags" not in tags:  # Check for the 'tags' key
+                    raise ValidationError(
+                        _(
+                            "%(value)s The field dict must contain the key "
+                            "'tags' with a value type str or set[str] or "
+                            "list[str]. Keys supplied %(keys)s."
+                        ),
+                        params={
+                            "value": tags,
+                            "keys": tags.keys(),
+                        },
+                        code="invalid",
+                    )
+                return True  # Valid dictionary
+            case list() | set() | str() | None:
+                return True
+            case _:  # Catch-all for invalid types
+                return False
 
-                        case _:
+    def _is_valid_tag_list(self, tag_list: list[str]) -> bool:
+        """Checks if all tags in a list are valid."""
+        return all(self._is_valid_tag(tag) for tag in tag_list)
+
+    def _extract_tags_from_dict(self, tags: dict[list[str] | str]) -> list:
+        """Extracts and validates tags from a dictionary.
+
+        :param tags: The input dictionary.
+
+        :returns: A list of valid tags.
+
+        :raises ValidationError: If the 'tags' key is missing, the value
+                                 associated with 'tags' is invalid, or if an
+                                 error occurs during tag validation.
+        """
+        try:
+            if self._is_valid_tag_container(tags):
+                inner_tags: str | list = tags["tags"]
+                match inner_tags:
+                    case str():
+                        return parse_tags(inner_tags)
+                    case list() | set():
+                        if self._is_valid_tag_list(inner_tags):
+                            return inner_tags
+                        else:
                             raise ValidationError(
                                 _(
-                                    "%(value)s The field dict must contain a value type str or set[str] or list[str]. Keys supplied %(keys)s. Value type %(values)s."  # noqa: E501
+                                    "%(value)s The dict must contain a value "
+                                    "type str or set[str] or list[str]. Keys "
+                                    "supplied %(keys)s. Value type %(values)s."
                                 ),
                                 params={
                                     "value": tags,
@@ -155,30 +204,120 @@ class FieldTagListFormatter(list):
                                 },
                                 code="invalid",
                             )
+                    case _:  # Handles unexpected types
+                        raise ValidationError(
+                            _(
+                                "%(value)s The dict must contain a value type "
+                                "str or set[str] or list[str]. Keys supplied "
+                                "%(keys)s. Value type %(values)s."
+                            ),
+                            params={
+                                "value": tags,
+                                "keys": tags.keys(),
+                                "values": type(tags.get("tags")),
+                            },
+                            code="invalid",
+                        )
+        except ValidationError as e:
+            self.logger.error("An invalid dictionary was passed %s", e)
+            return []
 
+    def _get_tag_list(
+        self,
+        tags: (
+            dict[str : list[str] | set[str] | str]  # noqa: E203
+            | list[str]
+            | set[str]
+            | str
+            | None
+        ) = None,
+    ) -> list[str]:
+        """
+        Extracts a list of valid tags from various input types.
+
+        Handles the following input types:
+
+        * **dict:** Expects a dictionary where the value associated with the 'tags' key # noqa: E501
+           is a list, set, or string of tags.
+        * **list or set:**  Expects a list or set directly containing string tags. # noqa: E501
+        * **str:** Expects a string containing tags (presumably delimited in some way). # noqa: E501
+        * **None:** Returns an empty list.
+
+        :param tags: The input to extract tags from. Can be a dictionary, list, set, string, or None. # noqa: E501
+        :returns: A list of valid tags extracted from the input.
+        :raises ValidationError: If the input `tags` is of an invalid type or the tags within are invalid. # noqa: E501
+        """
+        if tags is None:
+            return []
+
+        match tags:
+            case dict():
+                return self._extract_tags_from_dict(tags)
+            case list() | set():
+                if self._is_valid_tag_list(tags):
+                    return tags
                 else:
                     raise ValidationError(
                         _(
-                            "%(value)s The field dict must contain the key 'tags' with a value type str or set[str] or list[str]. Keys supplied %(keys)s."  # noqa: E501
+                            "%(value)s must be dict or list or set containing "
+                            "strings, or a string or None, type is "
+                            "%(val_type)s"
                         ),
-                        params={
-                            "value": tags,
-                            "keys": tags.keys(),
-                        },
+                        params={"value": tags, "val_type": type(tags)},
                         code="invalid",
                     )
-            case list():
-                return tags
-            case None:
-                return []
-            case set():
-                return list(tags)
             case str():
                 return parse_tags(tags)
             case _:
                 raise ValidationError(
                     _(
-                        "%(value)s must be dict or list or set containing strings, or a string or None, type is %(val_type)s"  # noqa: E501
+                        "%(value)s must be dict or list or set containing "
+                        "strings, or a string or None, type is %(val_type)s"
+                    ),
+                    params={"value": tags, "val_type": type(tags)},
+                    code="invalid",
+                )
+
+    def _add_tags(
+        self,
+        tags: (
+            dict[str : list[str] | set[str] | str]  # noqa: E203
+            | list[str]
+            | set[str]
+            | str
+            | None
+        ) = None,
+    ) -> list[str]:
+        """Adds valid, unique tags to the internal tag list and returns the sorted list. # noqa: E501
+
+        Handles the following input types:
+
+        * **dict:** Expects a dictionary where the value associated with the 'tags' key # noqa: E501
+           is a list, set, or string of tags.
+        * **list or set:**  Expects a list or set directly containing string tags. # noqa: E501
+        * **str:** Expects a string containing tags (presumably delimited in some way). # noqa: E501
+        * **None:** Returns an empty list.
+
+
+        :param tags: The input to extract tags from. Can be a dictionary, list, set, string, or None. # noqa: E501
+        :returns: A sorted list of unique, valid tags added to the object's internal state. # noqa: E501
+        :raises ValidationError: If the input `tags` is of an invalid type or the tags within are invalid. # noqa: E501
+        """
+        try:
+            if self._is_valid_tag_container(tags):
+                tag_list = self._get_tag_list(tags)
+
+                for tag in tag_list:
+                    if self._is_valid_tag(tag) and tag not in self.tags:
+                        self.tags.append(tag)
+
+                return sorted(self.tags)
+
+            else:
+                raise ValidationError(
+                    _(
+                        "%(value)s must be dict or list or set containing "
+                        "strings, or a string or None, type is %(val_type)s"
                     ),
                     params={
                         "value": tags,
@@ -187,53 +326,27 @@ class FieldTagListFormatter(list):
                     code="invalid",
                 )
 
-    def _add_tags(
-        self,
-        tags: dict[str : list[str] | set[str] | str]  # noqa: E203
-        | list[str]
-        | set[str]
-        | str
-        | None = None,
-    ) -> list[str]:
-        """Return a list of unique tags, without any null values."""
+        except ValidationError as e:
 
-        tag_list = self._get_tag_list(tags)
-        null_tags: list = [
-            "null",
-            "Null",
-            "NULL",
-        ]
+            self.logger.error(
+                "An invalid tag or container was passed %s",
+                e,
+            )
 
-        if tag_list:
-            for tag in tag_list:
-                match tag:
-                    case str():
-                        if tag not in self.tags and tag not in null_tags:
-                            self.tags.append(tag)
-                    case _:
-                        raise ValidationError(
-                            _(
-                                "%(value)s must be type <class 'str'>, type is %(val_type)s"  # noqa: E501
-                            ),
-                            params={
-                                "value": tag,
-                                "val_type": type(tag),
-                            },
-                            code="invalid",
-                        )
-
-        return sorted(self.tags)
+            return []
 
     def add_tags(
         self,
-        tags: dict[str : list[str] | set[str] | str]  # noqa: E203
-        | list[str]
-        | set[str]
-        | str
-        | None = None,
+        tags: (
+            dict[str : list[str] | set[str] | str]
+            | list[str]
+            | set[str]
+            | str
+            | None
+        ) = None,
     ) -> list[str]:
         """Allows addition of multiple tags to the tags list."""
-        self._add_tags(tags)
+        return self._add_tags(tags)
 
     def append(self, item):
         """S.append(value) -- append value to the end of the sequence"""
@@ -248,11 +361,13 @@ class FieldTagListFormatter(list):
 
     def del_tags(
         self,
-        tags: dict[str : list[str] | set[str] | str]  # noqa: E203
-        | list[str]
-        | set[str]
-        | str
-        | None = None,
+        tags: (
+            dict[str : list[str] | set[str] | str]  # noqa: E203
+            | list[str]
+            | set[str]
+            | str
+            | None
+        ) = None,
     ) -> list[str]:
         """Allows deletion of multiple tags from tags list."""
         tag_list = self._get_tag_list(tags)
@@ -265,11 +380,13 @@ class FieldTagListFormatter(list):
 
     def extend(
         self,
-        tags: dict[str : list[str] | set[str] | str]  # noqa: E203
-        | list[str]
-        | set[str]
-        | str
-        | None = None,
+        tags: (
+            dict[str : list[str] | set[str] | str]  # noqa: E203
+            | list[str]
+            | set[str]
+            | str
+            | None
+        ) = None,
     ) -> list[str]:
         """S.extend(dict, list, set or string) -- extend tags list by appending elements"""  # noqa: E501
         return self.add_tags(tags)
