7930bf71180bfb40bd7e824cd4cc83b1b9ef8d8c
mark sevelj <31756570+imAsparky@users.noreply.github.com>
2024-03-09 12:07:50 +0800

    Revert "refactor(chars): Update model/form custom Charfields #48"
    This reverts commit 6c1efadf3137c9647df8ad0c42b3a2d6a03be0e3.

--- tag_me/db/models/fields.py
+++ tag_me/db/models/fields.py
@@ -1,6 +1,5 @@
 """tag-me app collections."""
 
-from django.core import validators
 from django.db.models.fields import CharField
 
 from tag_me.db.forms.fields import TagMeCharFieldForm
@@ -8,72 +7,46 @@ from tag_me.utils.collections import FieldTagListFormatter
 
 
 class TagMeCharField(CharField):
-    """A custom Django model field for storing and managing tags.
+    """A tagged field inheriting from Charfield."""
 
-    This field extends the built-in CharField and utilizes a
-    FieldTagListFormatter instance internally to provide tag validation,
-    formatting, and manipulation. Tags are stored in the database sorted, and
-     in a comma-separated (CSV)  format.
-    """
-
-    def __init__(self, *args, db_collation=None, **kwargs):
-        """
-        Initializes the TagMeCharField.
-
-        :param *args: Positional arguments passed to the parent CharField constructor. # noqa: E501
-        :param **kwargs: Keyword arguments passed to the parent CharField constructor. # noqa: E501
-        """
-        super().__init__(*args, **kwargs)
-        self.db_collation = db_collation
-        if self.max_length is not None:
-            self.validators.append(
-                validators.MaxLengthValidator(self.max_length)
-            )
-        self.formatter = FieldTagListFormatter()
-
-    def from_db_value(self, value, expression, connection):
-        """
-        Converts the database representation of tags into a FieldTagListFormatter. # noqa: E501
-
-        :param value: The raw tag data as retrieved from the database (expected to be a CSV string). # noqa: E501
-        :param expression: Information about how the value was obtained (e.g., aggregations). # noqa: E501
-        :param connection: The database connection used.
-
-        :return: A FieldTagListFormatter instance containing the parsed tags.
-        """
-
-        print(f"FIELD MODEL FROM DB VALUE: {value}")
-        self.formatter.add_tags(value)
-        value = self.formatter.toCSV()
-
-        return value
-
-    def get_prep_value(self, value):
-        """
-        Prepares the tag data for saving into the database.
-
-        :param value: The tag data, either as a FieldTagListFormatter instance or a raw string. # noqa: E501
-
-        :return: A CSV-formatted string representing the tags, ready for database storage. # noqa: E501
-        """
-        self.formatter.add_tags(value)
-        value = self.formatter.toCSV()
-
-        return value
+    def __init__(
+        self,
+        *args,
+        choices=None,
+        db_collation=None,
+        max_length=None,
+        **kwargs,
+    ):
+        self.choices = choices
+        if max_length is None:
+            self.max_length = 255
+        else:
+            self.max_length = max_length
+        super().__init__(
+            *args,
+            choices=choices,
+            db_collation=db_collation,
+            max_length=max_length,
+            **kwargs,
+        )
 
     def to_python(self, value):
+        """Custom tag-me Charfield.
+
+        Overrides to_python converting values into a
+        :class: FieldTagListFormatter, returning the :method: toCSV() string
+        in the format that :class: tag_me.utils.parser.parse_tags expects.
         """
-        Converts raw tag input into a FieldTagListFormatter.
 
-        This method is primarily used during form handling to transform input data. # noqa: E501
+        if self.choices:
+            return value
 
-        :param value: The raw tag data, typically a string.
-
-        :return: A FieldTagListFormatter instance containing the parsed tags.
-        """
-        print(f"FIELD MODEL TO PYTHON VALUE: {value}")
-        self.formatter.add_tags(value)
-        value = self.formatter.toCSV()
+        match value:
+            case None:
+                value = ""
+                return value
+            case _:
+                value = FieldTagListFormatter(value).toCSV()
 
         return value
 
--- tag_me/tag_me/views/generic/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-thats excellent, can you please create doc string suitable for a new django or python user for the class and function. Can you please make reference to it being a custom overridden view to assit django-tag-me integration. Please use the :params: style
-"""
