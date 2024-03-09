5b25528faa381d55232d7d11337ea754966af4f8
imAsparky <mark.sevelj@dunwright.com.au>
2024-03-09 13:51:26 +0800

    chore(tag): Add custom form #51
    closes #51

--- tag_me/forms.py
+++ tag_me/forms.py
@@ -5,6 +5,19 @@ from django import forms
 from tag_me.models import UserTag
 
 
+class TagCustomModelForm(forms.ModelForm):
+    """Custom tag-me model form."""
+
+    def __init__(self, *args, **kwargs):
+        # # Add user_id to form
+        self.user = kwargs.pop("user", None)
+        self.fields["port_tags"].model_verbose_name = kwargs.pop(
+            "model_verbose_name", None
+        )
+        print(f'FORM INIT Verbose {self.fields["port_tags"].__dict__}')
+        super().__init__(*args, **kwargs)
+
+
 class UserTagsBaseForm(forms.ModelForm):
     """Business creation form."""
 
@@ -25,9 +38,9 @@ class UserTagsBaseForm(forms.ModelForm):
             ...
         else:
             for field in tagged_fields:
-                self.fields[
-                    field
-                ].widget.template_name = "clearable_file_input.html"
+                self.fields[field].widget.template_name = (
+                    "clearable_file_input.html"
+                )
 
                 # This will dynamically load the choices in the field
                 self.fields[field].widget.choices = [
