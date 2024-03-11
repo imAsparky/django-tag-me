d097b4d560d6eb714f1cb1758a498aeb4addebbc
imAsparky <mark.sevelj@dunwright.com.au>
2024-03-11 10:55:11 +0800

    refactor(form): Update the custom form field name #58
    closes #58

--- tests/test_form_field.py
+++ tests/test_form_field.py
@@ -3,23 +3,23 @@
 from django.core import validators
 from django.test import SimpleTestCase
 
-from tag_me.db.forms.fields import TagMeCharFieldForm
+from tag_me.db.forms.fields import TagMeCharField
 from tag_me.utils.collections import FieldTagListFormatter
 
 
 class TestTagMeCharFieldForm(SimpleTestCase):
     def test_empty_value(self):
-        f = TagMeCharFieldForm()
+        f = TagMeCharField()
 
         assert f.to_python("") == ""
 
     def test_with_value(self):
-        f = TagMeCharFieldForm()
+        f = TagMeCharField()
 
         assert f.to_python("a,b,c") == FieldTagListFormatter("a,b,c").toCSV()
 
     def test_validators(self):
-        f = TagMeCharFieldForm(
+        f = TagMeCharField(
             min_length=1234,
             max_length=5678,
         )
