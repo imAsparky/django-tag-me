e16389226a0db47b36def9285cb179c6af51f7f6
imAsparky <mark.sevelj@dunwright.com.au>
2024-03-11 10:53:47 +0800

    chore(default): Add widget to formfield #58
    Add the default widget to formfield.  This negates the need for custom views and dynamic assignment of the widget in forms.
    
    Refactor the custom form Charfield
    
    closes #58

--- tag_me/db/forms/fields.py
+++ tag_me/db/forms/fields.py
@@ -1,18 +1,18 @@
 """tag-me Form Fields"""
 
-import logging
-from django.core import validators
+# import logging
+
+# from django.core import validators
 from django.forms.fields import CharField
 from django.utils.translation import gettext_lazy as _
 
-from tag_me.widgets import TagMeSelectMultipleWidget
-
 from tag_me.utils.collections import FieldTagListFormatter
 
-logger = logging.getLogger(__name__)
+
+# logger = logging.getLogger(__name__)
 
 
-class TagMeCharFieldForm(CharField):
+class TagMeCharField(CharField):
     """Custom tag-me Charfield Form
 
     Overrides to_python converting values into a
@@ -24,35 +24,13 @@ class TagMeCharFieldForm(CharField):
         "invalid": _("Please enter a valid tag/tags"),
     }
 
-    def __init__(
-        self,
-        *,
-        max_length=None,
-        min_length=None,
-        strip=True,
-        empty_value="",
-        **kwargs,
-    ):
-        self.max_length = max_length
-        self.min_length = min_length
-        self.strip = strip
-        self.empty_value = empty_value
-        super().__init__(**kwargs)
-        if min_length is not None:
-            self.validators.append(
-                validators.MinLengthValidator(int(min_length))
-            )
-        if max_length is not None:
-            self.validators.append(
-                validators.MaxLengthValidator(int(max_length))
-            )
-        self.validators.append(validators.ProhibitNullCharactersValidator())
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
         self.formatter = FieldTagListFormatter()
-        self.widget = TagMeSelectMultipleWidget()
 
     def to_python(self, value):
         """Return FieldTagListFormatter(value).toCSV() string."""
 
-        print(f'FIELD FORM TO PYTHON VALUE: {value}')
         self.formatter.add_tags(value)
         return self.formatter.toCSV()
