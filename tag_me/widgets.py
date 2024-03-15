e82727c41bcbcd37673a1d9cbfd6e9b17e68f220
imAsparky <mark.sevelj@dunwright.com.au>
2024-03-15 18:51:56 +0800

    refactor(tags): Add tighter integration with template #56
    closes #56

--- tag_me/widgets.py
+++ tag_me/widgets.py
@@ -4,6 +4,7 @@ from typing import override
 
 from django import forms
 from django.contrib.auth import get_user_model
+from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
 
 from tag_me.utils.helpers import get_user_field_choices_as_list_tuples
@@ -62,55 +63,95 @@ class TagMeSelectMultipleWidget(forms.SelectMultiple):
             user=User.objects.get(username=user),
         )
 
-        # ... (Code for generating the HTML output, default Django select) ...
-        output = [f'<select multiple name="{name}" class={css_class}>']
-        for option_value, option_label in self.choices:
-            selected = option_value in value
-            output.append(
-                '<option value="{}" {}>{}</option>'.format(
-                    option_value, "selected" if selected else "", option_label
-                )
-            )
-        output.append("</select>")
-        return mark_safe("".join(output))
+        values: list = []
+        match value:
+            case str():
+                for val in value.rstrip(",").split(","):
+                    values.append(val.strip())
 
-    # @override
-    # def optgroups(self, name, value, attrs=None):
-    #     """Return a list of optgroups for this widget."""
-    #     groups = []
-    #     has_selected = False
-    #
-    #     for index, (option_value, option_label) in enumerate(self.choices):
-    #         if option_value is None:
-    #             option_value = ""
-    #
-    #         subgroup = []
-    #         if isinstance(option_label, (list, tuple)):
-    #             group_name = option_value
-    #             subindex = 0
-    #             choices = option_label
-    #         else:
-    #             group_name = None
-    #             subindex = None
-    #             choices = [(option_value, option_label)]
-    #         groups.append((group_name, subgroup, index))
-    #
-    #         for subvalue, sublabel in choices:
-    #             selected = (
-    #                 not has_selected or self.allow_multiple_selected
-    #             ) and str(subvalue) in value[0]
-    #             has_selected |= selected
-    #             subgroup.append(
-    #                 self.create_option(
-    #                     name,
-    #                     subvalue,
-    #                     sublabel,
-    #                     selected,
-    #                     index,
-    #                     subindex=subindex,
-    #                     attrs=attrs,
-    #                 )
-    #             )
-    #             if subindex is not None:
-    #                 subindex += 1
-    #     return groups
+        context = {
+            "name": name,
+            "values": values,
+            "choices": self.choices,
+            # "options": json.dumps(options),
+        }
+
+        return mark_safe(render_to_string(self.template_name, context))
+
+
+#  -----------  Code for refactor and removal of <select> in html  -----------
+
+# options_list = get_user_field_choices_as_list_tuples(
+#     model_verbose_name=model_verbose_name,
+#     field_verbose_name=field_name,
+#     user=User.objects.get(username=user),
+# )
+
+# print(f"\n\nWIDGET VALUES: {value}  {type(value)}")
+# print(f"\n\nWIDGET CHOICES: {self.choices}  {type(self.choices)}")
+
+# # ... (Code for generating the HTML output, default Django select) ...
+# output = [f'<select multiple name="{name}" class={css_class}>']
+# for option_value, option_label in self.choices:
+#     selected = option_value in value
+#     output.append(
+#         '<option value="{}" {}>{}</option>'.format(
+#             option_value, "selected" if selected else "", option_label
+#         )
+#     )
+# output.append("</select>")
+
+# options: dict = []
+# option: dict = {}
+# for option in options_list:
+#     options.append(
+#         {
+#             "value": option,
+#             "text": option,
+#             "search": option,
+#             "selected": option in value,
+#         }
+#     )
+
+# ----------------------------------------------------------------------------
+
+# @override
+# def optgroups(self, name, value, attrs=None):
+#     """Return a list of optgroups for this widget."""
+#     groups = []
+#     has_selected = False
+#
+#     for index, (option_value, option_label) in enumerate(self.choices):
+#         if option_value is None:
+#             option_value = ""
+#
+#         subgroup = []
+#         if isinstance(option_label, (list, tuple)):
+#             group_name = option_value
+#             subindex = 0
+#             choices = option_label
+#         else:
+#             group_name = None
+#             subindex = None
+#             choices = [(option_value, option_label)]
+#         groups.append((group_name, subgroup, index))
+#
+#         for subvalue, sublabel in choices:
+#             selected = (
+#                 not has_selected or self.allow_multiple_selected
+#             ) and str(subvalue) in value[0]
+#             has_selected |= selected
+#             subgroup.append(
+#                 self.create_option(
+#                     name,
+#                     subvalue,
+#                     sublabel,
+#                     selected,
+#                     index,
+#                     subindex=subindex,
+#                     attrs=attrs,
+#                 )
+#             )
+#             if subindex is not None:
+#                 subindex += 1
+#     return groups
