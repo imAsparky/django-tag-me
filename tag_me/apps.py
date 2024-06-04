b57ab14cd39935bbc9290d8dfd501f8aed6a76a2
imAsparky <mark.sevelj@dunwright.com.au>
2024-06-04 11:19:49 +0800

    feat(theme): Add dropdown theme functionality #119
    Add setting to allow key theme and path value to choose a theme.
    The theme is can be selected on a per field basis in the form init and
    assign the theme name to the field widget.
    Default tag_me theme is added if the dev does not create new themes.
    
    closes #119

--- tag_me/apps.py
+++ tag_me/apps.py
@@ -1,13 +1,29 @@
 """django-tag-me Apps file."""
 
+from typing import override
 from django.apps import AppConfig
+from django.conf import settings
 from django.utils.translation import gettext_lazy as _
+from django.utils.translation.trans_real import settings
 
 
-class AppNameConfig(AppConfig):
+class DjangoTagMeConfig(AppConfig):
     """tag_me App name config."""
 
     default_auto_field = "django.db.models.BigAutoField"
     name = "tag_me"
     verbose_name = _("Django Tag Me")
     verbose_name_plural = _("Django Tag Me")
+
+    @override
+    def ready(self):
+        if not hasattr(settings, "DJ_TAG_ME_THEMES"):
+            settings.DJ_TAG_ME_THEMES = {
+                "default": "tag_me/tag_me_select.html",
+            }
+        if not hasattr(settings, "DJ_TAG_ME_MAX_NUMBER_DISPLAYED"):
+            settings.DJ_TAG_ME_MAX_NUMBER_DISPLAYED = 2
+
+       # return super().ready()
+
+
