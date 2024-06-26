d0a6406d8c5ec2d4f40a80c626729ddaedb67fae
imAsparky <mark.sevelj@dunwright.com.au>
2024-06-26 15:21:12 +0800

    chore(tags): Add example tag management app #131
    A very basic app listing all user tags and the system generated
    models with tagged fields.
    
    closes #131

---
+++ example/blog/tags/forms.py
@@ -0,0 +1,82 @@
+"""example tags forms."""
+
+from django.db.models.fields import forms
+from tag_me.db.forms.mixins import TagMeModelFormMixin
+from tag_me.models import (
+    UserTag,
+    TaggedFieldModel,
+)
+from django.utils.translation import pgettext_lazy as _
+
+
+class UserTagListForm(forms.ModelForm):
+    """User tag creation form"""
+
+    class Meta:
+        model = UserTag
+        fields = [
+            "user",
+            "content_type",
+            "model_verbose_name",
+            "field_name",
+            "name",  # This is the tag.
+        ]
+
+        labels = {
+            "user": _(
+                "Label",
+                "User",
+            ),
+            "content_type": _(
+                "Label",
+                "Application",
+            ),
+            "model_verbose_name": _(
+                "Label",
+                "Feature",
+            ),
+            "field_name": _(
+                "Label",
+                "Field",
+            ),
+            "name": _(
+                "Label",
+                "Tag name",
+            ),
+        }
+
+
+class TaggedFieldsListForm(forms.ModelForm):
+    """Tagged model fields list"""
+
+    class Meta:
+        model = TaggedFieldModel
+        fields = [
+            "content",
+            "model_name",
+            "model_verbose_name",
+            "field_name",
+            "field_verbose_name",
+        ]
+        labels = {
+            "content": _(
+                "Label",
+                "App/Content name",
+            ),
+            "model_verbose_name": _(
+                "Label",
+                "Model Verbose/Feature name",
+            ),
+            "model_name": _(
+                "Label",
+                "Model name",
+            ),
+            "field_name": _(
+                "Label",
+                "Field name",
+            ),
+            "field_verbose_name": _(
+                "Label",
+                "Field verbose name",
+            ),
+        }
---
+++ example/blog/tags/urls.py
@@ -0,0 +1,33 @@
+"""example tags management"""
+
+from django import urls
+from django.urls import (
+    include,
+    path,
+)
+from tags.views import (
+    tags_dashboard,
+    ListAllUserTagsView,
+    ListAllTaggedFieldView,
+)
+
+app_label = "tags"
+app_name = "tags"
+
+urlpatterns = [
+    path(
+        "",
+        tags_dashboard,
+        name="dashboard",
+    ),
+    path(
+        "list-all-tags/",
+        ListAllUserTagsView.as_view(),
+        name="list-all-tags",
+    ),
+    path(
+        "list-tagged-fields/",
+        ListAllTaggedFieldView.as_view(),
+        name="list-tagged-fields",
+    ),
+]
