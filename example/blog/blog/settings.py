aaa7cbbf00e5c8319f29fa94bb0ff64238f22e4d
imAsparky <mark.sevelj@dunwright.com.au>
2024-06-27 14:05:58 +0800

    chore(conf): Update login redirect #130

--- example/blog/blog/settings.py
+++ example/blog/blog/settings.py
@@ -129,5 +129,5 @@ STATIC_ROOT = Path(BASE_DIR) / "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_REDIRECT_URL = "landing"
-LOGOUT_REDIRECT_URL = "login"
+LOGIN_REDIRECT_URL = "/landing"
+LOGOUT_REDIRECT_URL = "/login"
