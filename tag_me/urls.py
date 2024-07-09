d1c09aa6a6a5322831c459e183de3131c02d8ba7
imAsparky <mark.sevelj@dunwright.com.au>
2024-07-09 15:45:58 +0800

    chore(mgmt): Add tag management #139
    Add the views and urls for the tag management
    interface.
    
    closes #139

--- tag_me/urls.py
+++ tag_me/urls.py
@@ -1,13 +1,40 @@
 """django-tag-me Url's file."""
 
-from django.urls import include, path
+from django.urls import path
 
-from .views import UserTagCreateView, UserTagListView
+from .views import (
+    SynchronisedTagFieldListView,
+    TagFieldListView,
+    TagManagementView,
+    UserTagCreateView,
+    UserTagDeleteView,
+    UserTagDetailView,
+    UserTagListView,
+    UserTagUpdateView,
+)
 
 # from .views import fnc
 
 app_label = "tag_me"
 app_name = "tag_me"
+
+mgmtpatterns = [
+    path(
+        "mgmt/",
+        TagManagementView.as_view(),
+        name="tag-mgmt",
+    ),
+    path(
+        "tagged-field-list/",
+        TagFieldListView.as_view(),
+        name="tagged-field-list",
+    ),
+    path(
+        "sync-tagged-field-list/",
+        SynchronisedTagFieldListView.as_view(),
+        name="sync-tagged-field-list",
+    ),
+]
 
 urlpatterns = [
     path(
@@ -16,8 +43,25 @@ urlpatterns = [
         name="create-tag",
     ),
     path(
+        "delete/tag/<int:pk>/",
+        UserTagDeleteView.as_view(),
+        name="delete-tag",
+    ),
+    path(
+        "detail/tag/<int:pk>/",
+        UserTagDetailView.as_view(),
+        name="detail-tag",
+    ),
+    path(
         "list/tag/",
         UserTagListView.as_view(),
         name="list-tags",
     ),
+    path(
+        "update/tag/<int:pk>/",
+        UserTagUpdateView.as_view(),
+        name="update-tag",
+    ),
 ]
+
+urlpatterns.extend(mgmtpatterns)
