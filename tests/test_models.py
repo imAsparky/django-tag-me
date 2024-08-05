5e42936b8be60248cf24122459842f39dc064884
imAsparky <mark.sevelj@dunwright.com.au>
2024-08-05 11:27:48 +0800

    test(tags): Update tests #167

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,24 +1,15 @@
 """tag-me model tests"""
 
 import logging
-import re
 import string
 
-import pytest
-from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
-from django.utils.text import slugify
 from hypothesis import given
 from hypothesis import strategies as st
-from hypothesis.extra.django import TestCase, from_model
+from hypothesis.extra.django import TestCase
 
 from tests.models import TaggedFieldTestModel
 
-# from tag_me.models import TagBase
-
 logger = logging.getLogger(__name__)
-
-# PROBABLY REDUNDENT TEST NOW
 
 
 class TestTag(TestCase):
@@ -36,30 +27,29 @@ class TestTag(TestCase):
         tag = "asdf"
         assert tag in model.slugify(tag)
 
+    @given(
+        st_name=st.text(
+            # alphabet=st.characters(
+            #     blacklist_categories=[
+            #         "Cs",
+            #         "Cc",
+            #     ],
+            #     # codec="ascii",
+            # ),
+            alphabet=string.ascii_letters,
+            min_size=1,
+            max_size=50,
+        )
+    )
+    def test_base_created_no_unicode_ok(
+        self,
+        st_name,
+    ):
+        tag = TaggedFieldTestModel.objects.create(
+            tags=st_name,
+        )
 
-#     @given(
-#         st_name=st.text(
-#             # alphabet=st.characters(
-#             #     blacklist_categories=[
-#             #         "Cs",
-#             #         "Cc",
-#             #     ],
-#             #     # codec="ascii",
-#             # ),
-#             alphabet=string.ascii_letters,
-#             min_size=1,
-#             max_size=50,
-#         )
-#     )
-#     def test_base_created_no_unicode_ok(
-#         self,
-#         st_name,
-#     ):
-#         tag = TaggedFieldTestModel.objects.create(
-#             tags=st_name,
-#         )
-#         print(f"TAG {slugify(tag.tags)}")
-#         print(f"SLUGGING IT {tag.slug}")
-#         # assert TagBase.slugify(tag.tags) in tag.slug
-#         assert slugify(tag.tags) in tag.slug
-#         assert len(tag.slug) >= 8
+        # Note: We need to do some work on the slugify process first...
+        # assert TagBase.slugify(tag.tags) in tag.slug
+
+        assert len(tag.slug) >= 8
